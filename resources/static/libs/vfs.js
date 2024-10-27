716dbea88294e4c332b0e9951be94e370b6c3b6a
skwyarspro15 <potatobg648@gmail.com>
2024-10-27 23:09:52 +0800

    Listing update

--- resources/static/libs/vfs.js
+++ resources/static/libs/vfs.js
@@ -26,19 +26,6 @@ let templateFsLayout = {
               },
             },
             {
-              id: "audioplayer",
-              name: "Audio Player (WIP)",
-              color: "hsl(15, 65%, 62%)",
-              lastPlayed: Date.now() - 1_000,
-              image:
-                "data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='24' height='24' viewBox='0 0 24 24' fill='none' stroke='white' stroke-width='2' stroke-linecap='round' stroke-linejoin='round' class='lucide lucide-speaker'%3E%3Crect width='16' height='20' x='4' y='2' rx='2'/%3E%3Cpath d='M12 6h.01'/%3E%3Ccircle cx='12' cy='14' r='4'/%3E%3Cpath d='M12 14h.01'/%3E%3C/svg%3E",
-              launchPkg: "apps:AudioPlayer",
-              preview: {
-                title: "Audio Player",
-                description: "Listen to audio files.",
-              },
-            },
-            {
               id: "filemanager",
               name: "File Manager",
               color: "hsl(130, 65%, 62%)",
